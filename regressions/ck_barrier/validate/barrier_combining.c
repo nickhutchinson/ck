99b6c02978c9ea218bfc8feeb7f9b438df7a8187
Nick Hutchinson <nshutchinson@gmail.com>
2026-09-19 14:45:29 +0100

    msvc compat: don't assume presence of strings.h

--- regressions/ck_barrier/validate/barrier_combining.c
+++ regressions/ck_barrier/validate/barrier_combining.c
@@ -32,7 +32,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
 
--- regressions/ck_hp/validate/nbds_haz_test.c
+++ regressions/ck_hp/validate/nbds_haz_test.c
@@ -36,7 +36,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
 
